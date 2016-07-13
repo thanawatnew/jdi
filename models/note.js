3b329b4588414b3c0e5f4668fd4c446bfd17ef73
thanawatnew <thanawatnew@gmail.com>
2016-07-13 18:22:27 +0700

    add files

---
+++ models/note.js
@@ -0,0 +1,15 @@
+var mongoose    =   require("mongoose");
+mongoose.connect('mongodb://localhost:27017/jdiDb');
+
+// create instance of Schema
+var Schema =   mongoose.Schema;
+
+// create schema
+var noteSchema  = new Schema({
+"link" : String,
+"detail" : String,
+"userEmail" : String
+},{timestamps:true});
+
+// create model if not exists.
+module.exports = mongoose.model('note',noteSchema);
