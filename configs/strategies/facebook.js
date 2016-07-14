40f38367e4967dc6b83c002c4b29b2c16e633efb
thanawatnew <thanawatnew@gmail.com>
2016-07-14 17:25:34 +0700

    add passport-facebook strategy file

---
+++ configs/strategies/facebook.js
@@ -0,0 +1,70 @@
+var User     =   require("../../models/user");
+
+var passport = require('passport'),
+    url = require('url'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    config = require('../env');
+    //users = require('../../app/controllers/users.server.controller');
+
+module.exports = function() {
+    passport.use(new FacebookStrategy({
+        clientID: config.facebook.clientID,
+        clientSecret: config.facebook.clientSecret,
+        callbackURL: config.facebook.callbackURL,
+		profileFields: ['id', 'email','name'],
+        passReqToCallback: true
+    },
+    function(req, accessToken, refreshToken, profile, done) {
+        var providerData = profile._json;
+        providerData.accessToken = accessToken;
+        providerData.refreshToken = refreshToken;
+
+		console.log(profile);
+        var providerUserProfile = {
+            name: profile.name.givenName,
+            email: profile.emails[0].value,
+            username: profile.username,
+            provider: 'facebook',
+            providerId: profile.id,
+            providerData: providerData
+        };
+
+        saveOAuthUserProfile(req, providerUserProfile, done);//*/
+    }
+	));
+};
+
+saveOAuthUserProfile = function(req, profile, done) {
+    User.findOne({
+            provider: profile.provider,
+            providerId: profile.providerId
+        },
+        function(err, user) {
+            if (err) {
+            return done(err);
+            }
+            else {
+                if (!user) {
+                    var possibleUsername = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');
+                    User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
+                        profile.username = availableUsername;
+                        user = new User(profile);
+
+                        user.save(function(err) {
+                            if (err) {
+                                var message = _this.getErrorMessage(err);
+                                req.flash('error', message);
+                                return res.redirect('/signup');
+                            }
+
+                            return done(err, user);
+                        });
+                    });
+                }
+                else {
+                    return done(err, user);
+                }
+            }
+        }
+    );
+};
