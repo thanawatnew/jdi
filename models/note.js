6f167ec9e6816391306e8e66e5beb60b039c9be2
thanawatnew <thanawatnew@gmail.com>
2016-07-14 17:23:41 +0700

    use createConnection to prevent an same-time-opening-db error

--- models/note.js
+++ models/note.js
@@ -1,5 +1,11 @@
 var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://localhost:27017/jdiDb');
+mongoose.createConnection('mongodb://localhost:27017/jdiDb');
+/*
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", function(callback){
+	console.log("Connection Succeeded."); /* Once the database connection has succeeded, the code in db.once is executed. */
+//});
 
 // create instance of Schema
 var Schema =   mongoose.Schema;
---
+++ models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+mongoose.createConnection('mongodb://localhost:27017/jdiDb');
+
+var UserSchema = new Schema({
+    name: String,
+    userEmail: String,
+    username: String,
+    password: String,
+    accessToken: String,
+});
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+
+    _this.findOne(
+        {username: possibleUsername},
+        function(err, user) {
+            if (!err) {
+                if (!user) {
+                    callback(possibleUsername);
+                }
+                else {
+                    return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+                }
+            }
+            else {
+                callback(null);
+            }
+        }
+    );
+};
+
+// create model if not exists.
+module.exports = mongoose.model('user', UserSchema);
