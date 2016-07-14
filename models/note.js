ff3dea5c9d24e01ac8cef853a0e8f74db0effd1c
thanawatnew <thanawatnew@gmail.com>
2016-07-14 17:27:14 +0700

    delete a comment

--- models/note.js
+++ models/note.js
@@ -1,11 +1,5 @@
 var mongoose    =   require("mongoose");
 mongoose.createConnection('mongodb://localhost:27017/jdiDb');
-/*
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", function(callback){
-	console.log("Connection Succeeded."); /* Once the database connection has succeeded, the code in db.once is executed. */
-//});
 
 // create instance of Schema
 var Schema =   mongoose.Schema;
